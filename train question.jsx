7dc0b0e7b7ca2510da3fcecac530a6582fa14633
SabariRajendran2000 <109908702+SabariRajendran2000@users.noreply.github.com>
2023-07-26 11:28:54 +0530

    Add files via upload

---
+++ train question.jsx
@@ -0,0 +1,84 @@
+{
+    "companyName": "Train Central",
+    "ownerName": "Ram",
+    "rollNo": "1",
+    "ownerEmail":"ram@abc.edu",
+    "accessCode":"FKDLjg"
+    }
+    {
+    "companyName":"Train Central",
+    "clientID":"b46128a0-fbde-4c16-a4b1-6ae6ad718e27",
+    "ownerName":"Ram",
+    "ownerEmail":"ram@abc.edu",
+    "rollNo":"1",
+    "clientSecret":"XOyolORPayKBOdAN"
+    }
+    {
+    "token_type":"Bearer",
+    "access_token":
+    "eyJhbGciOiJIUzIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODI2ODI2MjQsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiYjQ2MTE4ZmJkZSOOYjE2LWE0YEtNmFlNmFkNzE8YjI3In0.V939QcxrZHWDTnTwm0-6ttoTGI4C65Grhn3rIJDC8fy8",
+    "expires_in":1682629264
+    }
+    [
+    {
+    "trainName":"Chennai Exp",
+    "trainNumber":"2344",
+    "departureTime":{
+    "Hours":21,
+    "Minutes":35,
+    "Seconds":0
+    },
+    "price":{
+    "sleeper":2,
+    "AC":1
+    },
+    "price":{
+    "sleeper":2,
+    "AC":5
+    },
+    "delayedBy":15
+    },
+    "delayedBy":15
+    },
+    {
+    "trainName":"Hyderabad Exp",
+    "trainNumber":"2341",
+    "departure": {
+    "Hours" : 23,
+    "Minutes": 55,
+    "Seconds" :0
+    },
+    "seatsAvailable":{
+    "sleeper":6,
+    "AC":7
+    },
+    "seatsAvailable":{
+    "sleeper":554,
+    "AC":1854
+    },
+    "delayedBy":5
+    }
+    ]
+    {
+    {
+    "trainName":"Delhi Door Hai Exp",
+    "trainNumber":"2343",
+    "departure": {
+    "Hours" : 9,
+    "Minutes": 45,
+    "Seconds" :0
+    },
+    "seatsAvailable":{
+    "sleeper":6,
+    "AC":7
+    },
+    "seatsAvailable":{
+    "sleeper":32,
+    "AC":1
+    },
+    "price":{
+    "sleeper":1,
+    "AC":723
+    },
+    "delayedBy":3
+    }
